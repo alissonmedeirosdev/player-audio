7b6654d04caae78686590e2cb465ed7d0f270b47
Alisson Melo <alissonmelo.work@gmail.com>
2021-10-03 17:26:34 -0300

    hard challenge finished

---
+++ hard/utils.js
@@ -0,0 +1,9 @@
+function secondsToMinutes(time) {
+  const minutes = Math.floor(time / 60)
+  const seconds = Math.floor(time % 60)
+  return `${('0' + minutes).slice(-2)}:${('0' + seconds).slice(-2)}`
+}
+
+const path = files => `files/${files}`
+
+export { path, secondsToMinutes }
